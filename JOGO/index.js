80e8b8282103baa0cc1932543f5ade2df3161032
Amanda Rothmann <162368529+rothmail@users.noreply.github.com>
2024-08-09 11:57:32 -0300

    Update index.js

--- JOGO/index.js
+++ JOGO/index.js
@@ -2,10 +2,11 @@ const rl = require('readline-sync');
 
 // VARIÁVEIS GLOBAIS
 let vida
+let inventario = []
 
 // MENU
 function menu() {
-    console.log('|| Seja-bem vindo/a!\n|| Ajude Farida abaroge, atleta de atletismo a chegar nas olimpíadas de Paris 2024!')
+    console.log('|| Seja-bem vindo/a!\n|| Ajude Farida Abaroge,\natleta de atletismo a chegar\nnas olimpíadas de Paris 2024!')
     console.log('_________||Ações||_________')
     console.log('|| 1. Explorar           ||')
     console.log('|| 2. Coletar Itens      ||')
@@ -15,6 +16,7 @@ function menu() {
     console.log('|| 6. Visualizar dados   ||')
     console.log('|| 7. Saber mais         ||')
     console.log('|| 0. Sair do jogo       ||')
+    console.log('**Algumas ações devem ser\nexecutadas para o jogo ter\ncontinuidade**')
 }
 menu()
 /*
@@ -28,7 +30,7 @@ function main () {
     }
 }
 */
-let opcao = rl.question('Escolha uma das ações!')
+let opcao = rl.question('Escolha uma das acoes!')
 
 // Escolha da ação da personasgem
 function escolhaMenu(opcao) {
@@ -60,42 +62,55 @@ function escolhaMenu(opcao) {
 
 
 // FUNÇÕES
-// função para explorar
+// OPÇÃO 1 - função para explorar
 function explorar() {
-    console.log('oi')
+    // etiópia (jimma), egito, sudão, frança
+    console.log('Vamos explorar alguns locais!')
+    let explorarJimma = rl.question('Farida nasceu em Jimma, Etiopia, deseja visitar a cidade (Este e o ponto de partida, caso escolha "nao", nao sera possivel avançar o nivel)? (S|N)')
+
+    if (explorarJimma === 'S') {
+        console.log('Explorando Jimma voce conquistou:\n|| +Nostalgia\n|| 10+Vida')
+        console.log(inventario.push('Nostalgia'))
+
+        coletarItem()
+    } else {
+        console.log('Não e possível avançar o nível. Tente novamente!')
+    }
 }
 
-// função para coletar itens
+// OPÇÃO 2 - função para coletar itens
 function coletarItem() {
     console.log('oi')
 }
 
-// função para interagir com NPCs
+// OPÇÃO 3 - função para interagir com NPCs
 function interagir() {
     console.log('oi')
 }
 
-// função para enfrentar desafios
+// OPÇÃO 4 - função para enfrentar desafios
 function enfrentarDesafio() {
     console.log('oi')
 }
 
-// função para abrir o inventário
+// OPÇÃO 5 - função para abrir o inventário
 function abrirInventario() {
-    console.log('oi')
+    console.log(inventario)
 }
 
-// função para abrir o inventário
+// OPÇÃO 6 - função para abrir o inventário
 function visualizarDados() {
     console.log('oi')
 }
 
-// função para abrir o inventário
-function abrirInventario() {
-    console.log('oi')
-}
-
-// função para saber mais sobre o jogo
+// OPÇÃO 7 - função para saber mais sobre o jogo
 function saberMais() {
-    console.log('oi')
+    console.log('________| O JOGO |________')
+    console.log('O jogo acompanha a trajetória de Farida Abaroge até as Olimpíadas de Paris 2024')
+
+    console.log('____| FARIDA ABAROGE |____')
+    console.log('|| Farida Abaroge de 30 anos e atleta olimpica de atletismo, participando da corrida de 100 metros.\n A atleta fugiu de seu pais por questoes de conflitos e viajou por paises como: Sudao, Egito e por fim Franca.')
+
+    console.log('_________| ACOES |_________')
+    console.log('|| Explorar - viaje para os países, Coletar Itens, Interagir, Desafio, Abrir inventário,Visualizar dados, Saber mais, Sair do jogo')
 }
